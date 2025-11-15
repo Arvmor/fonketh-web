55ecd93537b52a4cbaf744d75efec31c585c5cb0
Arvin Morawej <arvin.morawej@gmail.com>
2025-11-15 18:40:26 -0500

    feat: Add API Types

---
+++ packages/fonketh-sdk/src/types.ts
@@ -0,0 +1,47 @@
+/** Address type */
+export type Address = `0x${string}`;
+
+/** Hex Number type */
+export type HexNumber = `0x${string}`;
+
+/** Response status */
+export type ResponseStatus = 'success' | 'error';
+
+/** Schema for API Responses */
+export interface ResponseAPI<T> {
+  /** Response status */
+  status: ResponseStatus;
+  /** Response data */
+  data: T;
+}
+
+/** Mining Batch Schema */
+export type MiningBatch = [Address, HexNumber][];
+
+/** Position Schema */
+export interface Position {
+    /** X position */
+    x: number;
+    /** Y position */
+    y: number;
+}
+
+/** Players Schema */
+export interface Players {
+    /** Player name */
+    name: Address;
+    /** Player balance */
+    balance: number;
+    /** Player position */
+    position: Position;
+}
+
+/** Chat Message Schema */
+export interface ChatMessage {
+    /** Chat message identifier */
+    identifier: Address;
+    /** Chat message */
+    message: string;
+    /** Chat message timestamp */
+    timestamp: number;
+}
